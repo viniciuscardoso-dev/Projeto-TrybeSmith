d0e940edc5c53540074ac7b88b443a6e1121b175
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-06 20:25:00 -0300

    fix: rota /users

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import UserController from '../controllers/product.controller';
+import UserController from '../controllers/user.controller';
 
 const userController = new UserController(); 
 const router = Router();
 
-router.post('/', (req, res) => userController.newProduct(req, res));
+router.post('/', (req, res) => userController.newUser(req, res));
 
 export default router;
