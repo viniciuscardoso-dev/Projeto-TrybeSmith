8e7135994027a6057a61d6a1caa49e19fe59b3c6
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-06 19:13:06 -0300

    fix: product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,15 +1,14 @@
 import { Pool } from 'mysql2/promise';
-import connection from './connection';
 
-export default class BookModel {
-  private connection: Pool;
+export default class ProductModel {
+  public connection: Pool;
 
-  constructor() {
+  constructor(connection: Pool) {
     this.connection = connection;
   }
 
   public async getAll() {
-    const result = await this.connection.execute('SELECT * FROM Trybesmith.Users');
+    const result = await this.connection.execute('SELECT * FROM Trybesmith.Products');
     const [rows] = result;
     return rows;
   }
