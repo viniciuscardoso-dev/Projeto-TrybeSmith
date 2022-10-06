b5c15a2b2c1a4dfbb6c26943e33c9ca51f58fd32
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-06 19:19:45 -0300

    feat: rota POST /products

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -5,5 +5,6 @@ const productController = new ProductController();
 const router = Router();
 
 router.get('/', (req, res) => productController.getAll(req, res));
+router.post('/', (req, res) => productController.newProduct(req, res));
 
 export default router;
