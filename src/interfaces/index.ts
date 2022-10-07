5b20052afe936cdf581eb27ee72ebd82b7d16063
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-07 12:42:44 -0300

    feat: Generic Error class

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,13 +8,23 @@ export interface IProduct {
 export interface IUser {
   id?: number,
   username: string,
-  classe: string,
-  level: number,
-  password: string
+  classe?: string,
+  level?: number,
+  password?: string
 }
 
 export interface IOrder {
   id: number,
   userId: number,
   productsIds: number[],
+}
+
+export class GenericError extends Error {
+  // public details: { message: string }[];
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
