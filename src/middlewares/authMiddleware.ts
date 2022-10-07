496228ae9a19d73902692d17280797986d67d132
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-07 12:43:34 -0300

    fix: authMiddleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const { token } = req.headers;
 
   try {
-    // jwt.verify(token as string, process.env.JWT_SECRET);
-    jwt.verify(token as string, 'secret');
+    const secret = process.env.JWT_SECRET as string;
+    jwt.verify(token as string, secret);
     next();
   } catch (error) {
     next(error);
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+// import authMiddleware from '../middlewares/authMiddleware';
 
 const productController = new ProductController(); 
 const router = Router();
 
 router.get('/', (req, res) => productController.getAll(req, res));
+// router.use(authMiddleware);
 router.post('/', (req, res) => productController.newProduct(req, res));
 
 export default router;
